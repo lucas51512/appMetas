5710d55acc4e0a2eeb3df64c95b174f39eb2a470
Lucas <56892062+lucas51512@users.noreply.github.com>
2022-04-07 11:55:55 -0300

    V1 parcial Metas enviado

---
+++ appMetaV1/Meta.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace appMetaV1
+{
+    internal class Meta
+    {
+        private string nome;
+        private string descricao;
+        private bool atingido;
+        private int tempo;
+        private string tarefa;
+
+        Meta() 
+        {
+            nome = "Exemplo de Meta";
+            descricao = "Exemplo de Descrição";
+            atingido = false;
+            tempo = 0;
+            tarefa = "Exemplo de Tarefa";
+            
+        }
+
+        Meta(string Nome, string Descricao, int Tempo, string Tarefa) 
+        {
+            this.nome = Nome;
+            this.descricao = Descricao;
+            atingido = false;
+            this.tempo = Tempo;
+            this.tarefa = Tarefa;
+        }
+
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+
+        public string Descricao
+        {
+            get { return descricao; }
+            set { descricao = value; }
+        }
+
+        public bool Atingido
+        {
+            get { return atingido; }
+            set { atingido = value; }
+        }
+
+        public int Tempo
+        {
+            get { return tempo; }
+            set { tempo = value; }
+        }
+
+        public string Tarefa
+        {
+            get { return tarefa; }
+            set { tarefa = value; }
+        }
+
+        void Imprimir() 
+        {
+            Console.WriteLine("Meta: " + this.nome);
+        }
+    }
+}
